75474f924753930be6e7e1d6c0683707bc7a3280
Warith Hameen <100718903+Wahrith@users.noreply.github.com>
2024-03-26 13:00:07 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import MainPage from './MainPage/MainPage';
+function App() {
+  return (
+    <section className='App'>
+    <MainPage />
+    </section>
+  );
+}
+
+export default App;
