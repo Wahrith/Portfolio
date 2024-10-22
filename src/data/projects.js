dcb335031210b3feb764ee59029386d0ba541d05
Warith Hameen <100718903+Wahrith@users.noreply.github.com>
2024-10-22 15:48:44 +0100

    portfolio

---
+++ src/data/projects.js
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Car Showcase",
+    description: "Designed to help users make informed decisions about cars. Built using nextjs, typescript and tailwindcss.",
+    live: "https://car-showcase-hp8v.vercel.app/",
+    code: `https://github.com/Wahrith/Car_Showcase`,
+  },
+  {
+    id: 2,
+    title: "Movie Flight",
+    description:
+      "An app that retrieves movie data from an API and allows users to compare details of multiple searched movies.",
+    live: "https://movie-flight-2xg8.vercel.app/",
+    code: `https://github.com/Wahrith/movie_flight`,
+  },
+  {
+    id: 3,
+    title: "Tenzies Game",
+    description:
+      "A game with a 10 dice where a user keeps selecting a die till they are all the. Built with React and Css",
+    live: "https://tenzies-game-sage.vercel.app/",
+    code: `https://github.com/Wahrith/Tenzies_Game`,
+  },
+  {
+    id: 4,
+    title: "Memes Template",
+    description:
+      "An app where a user can generate personalized memes. Build with React and Css",
+    live: "https://memes-template.vercel.app/",
+    code: `https://github.com/Wahrith/Memes-Template`,
+  },
+
+];
